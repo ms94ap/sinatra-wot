6f848cdb47ab237554246b25ebced08ffcb40e99
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-16 20:17:48 +0000

    deleted join tables

--- db/migrate/20160716114454_create_types.rb
+++
@@ -1,7 +0,0 @@
-class CreateTypes < ActiveRecord::Migration[4.2]
-  def change
-    create_table :type do |t|
-      t.string :name #medium, light, tds, hv, spg
-    end
-  end
-end
--- db/migrate/20160716114952_create_tank_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateTankTypes < ActiveRecord::Migration[4.2]
-  def change
-    create_table :tank_type do |t|
-      t.integer :tank_id
-      t.integer :type_id
-    end
-
-  end
-end
