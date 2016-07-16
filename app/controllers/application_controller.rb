280c47bef9c7b59df39c457e344f7301eba19273
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-16 20:14:52 +0000

    removed helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,17 +15,4 @@ class ApplicationController < Sinatra::Base
     erb :main
   end
 
-
-
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
-
-
-end
+  
