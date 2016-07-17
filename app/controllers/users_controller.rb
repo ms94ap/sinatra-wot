f624d4994ed28bbe678a6a4b4b0080b262244e69
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-17 12:49:02 +0000

    create tanks/new form, added Tank Contro in conf.ru

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,48 @@
 class UsersController < ApplicationController
 
-get '/signup' do
-  if !logged_in?
-    erb :'users/signup'
-  else
-    erb :'/users/signup'
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      erb :'/users/signup'
+    end
   end
-end
 
-post '/signup' do
-  if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    redirect to '/signup'
-  else
-    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
-    @user.save
-   erb :'/users/welcome'
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+      @user.save
+     erb :'/users/welcome'
+    end
   end
-end
 
-get '/login' do
-  if !logged_in?
-    erb :'/users/login'
-  else
-    erb :'/users/welcome'
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      erb :'/users/welcome'
+    end
   end
-end
 
- post '/login' do
-  @user = User.find_by(username: params["username"])
-  if @user && @user.authenticate(params["password"])
-    session[:user_id] = @user.id
-    erb :'/users/welcome'
-  else
-    flash[:message] = "Wrong username and/or password"
-    redirect to '/'
+   post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      erb :'/users/welcome'
+    else
+      flash[:message] = "Wrong username and/or password"
+      erb :'/users/login'
+    end
   end
-end
 
-
-
-
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
 end
