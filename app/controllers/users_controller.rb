d770ce4316a435e2c22f1196b1560b97cdf056ed
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-18 08:32:51 +0000

    debug users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     else
       @user = User.new(username: params["username"], email: params["email"], password: params["password"])
       @user.save
+      session[:user_id] = @user.id
      erb :'/users/welcome'
     end
   end
